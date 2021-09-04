310b83c24f082f21187bfffc134f7c49be8bb72e
Mo_thunderz <76265418+mo-thunderz@users.noreply.github.com>
2021-09-04 15:50:55 +0200

    Add files via upload

--- Arduino/ESP32WifiNtp/ESP32WifiNtp.ino
+++ Arduino/ESP32WifiNtp/ESP32WifiNtp.ino
@@ -9,8 +9,9 @@
 #define UTC_OFFSET_IN_SECONDS 3600         // offset from greenwich time
 
 // SSID and password of Wifi connection:
-const char* ssid = "TYPE_YOUR_SSID_HERE";
-const char* password = "TYPE_YOUR_PASSWORD_HERE";
+//const char* ssid = "TYPE_YOUR_SSID_HERE";
+//const char* password = "TYPE_YOUR_PASSWORD_HERE";
+#include <WifiPassword.h>
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
@@ -34,6 +35,8 @@ void setup(){
 void loop() {
   timeClient.update();
 
+  // Option1: Get time and day of the week directly (discussed in Youtube video)
+  Serial.print("Option 1: ");
   Serial.print(daysOfTheWeek[timeClient.getDay()]);
   Serial.print(", ");
   Serial.print(timeClient.getHours());
@@ -42,5 +45,36 @@ void loop() {
   Serial.print(":");
   Serial.println(timeClient.getSeconds());
 
+  // Option 2: abstract from formatted date (added later as per request)
+  Serial.print("Option 2: ");
+  Serial.println(getTimeStampString());
+  
   delay(1000);
+}
+
+String getTimeStampString() {
+   time_t rawtime = timeClient.getEpochTime();
+   struct tm * ti;
+   ti = localtime (&rawtime);
+
+   uint16_t year = ti->tm_year + 1900;
+   String yearStr = String(year);
+
+   uint8_t month = ti->tm_mon + 1;
+   String monthStr = month < 10 ? "0" + String(month) : String(month);
+
+   uint8_t day = ti->tm_mday;
+   String dayStr = day < 10 ? "0" + String(day) : String(day);
+
+   uint8_t hours = ti->tm_hour;
+   String hoursStr = hours < 10 ? "0" + String(hours) : String(hours);
+
+   uint8_t minutes = ti->tm_min;
+   String minuteStr = minutes < 10 ? "0" + String(minutes) : String(minutes);
+
+   uint8_t seconds = ti->tm_sec;
+   String secondStr = seconds < 10 ? "0" + String(seconds) : String(seconds);
+
+   return yearStr + "-" + monthStr + "-" + dayStr + " " +
+          hoursStr + ":" + minuteStr + ":" + secondStr;
 }
